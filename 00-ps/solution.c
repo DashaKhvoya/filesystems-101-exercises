be5e2cf2bc29a6b4a54e95a00b920b7d79407944
Daria Baranchikova <dariabaranchikova@gmail.com>
2023-10-02 01:59:40 +0300

    add simple ps solution and gitignore

--- 00-ps/solution.c
+++ 00-ps/solution.c
@@ -1,6 +1,21 @@
 #include <solution.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+
+const char* processes_root_dir = "/proc";
 
 void ps(void)
 {
-	/* implement me */
+	DIR* root_dir = opendir(processes_root_dir);
+	if (!root_dir) {
+		report_error(processes_root_dir, errno);
+		return;
+	}
+	
+	struct dirent *entry;
+	while ((entry = readdir (root_dir)) != NULL) {
+		const char* name  = entry->d_name;
+		printf("pid = %d", name);
+  	}
 }
