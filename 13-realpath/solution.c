c4fc361542e21713bad6bc9d7fcf31a127c32f02
Daria Baranchikova <dariabaranchikova@gmail.com>
2023-10-15 14:58:24 +0300

    add realpath

--- 13-realpath/solution.c
+++ 13-realpath/solution.c
@@ -6,40 +6,83 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 void abspath(const char *input)
 {
-	if (input == NULL || strlen(input) < 1) {
+	if (input == NULL || strlen(input) < 1)
+	{
 		return;
 	}
 
 	char *path = strdup(input);
 	char *copy = path;
 	char result[PATH_MAX] = "";
+	char link[PATH_MAX] = "";
 
 	char *token, *saveptr;
 	while ((token = strtok_r(path, "/", &saveptr)) != NULL)
 	{
 		path = NULL;
 
-		char tmp[2*PATH_MAX] = "";
-		char full_path[PATH_MAX] = "";
-		snprintf(tmp, 2*PATH_MAX, "%s/%s", result, token);
-		realpath(tmp, full_path);
+		if (!strcmp(token, "."))
+		{
+			continue;
+		}
+		if (!strcmp(token, ".."))
+		{
+			if (strlen(result) > 1)
+			{
+				char *last = strrchr(result, '/');
+				*last = '\0';
+			}
+			continue;
+		}
+
+		char tmp_path[2 * PATH_MAX] = "";
+		snprintf(tmp_path, 2 * PATH_MAX, "%s/%s", result, token);
+
+		struct stat path_stat;
+		lstat(tmp_path, &path_stat);
 		if (errno)
 		{
 			report_error(result, token, errno);
 			free(copy);
 			return;
 		}
+		if (S_ISLNK(path_stat.st_mode))
+		{
+			// loop?
+			ssize_t link_len = readlink(tmp_path, link, PATH_MAX - 1);
+			if (errno)
+			{
+				report_error(result, token, errno);
+				free(copy);
+				return;
+			}
 
-		strncpy(result, full_path, PATH_MAX);
+			if (link[0] == '/')
+			{
+				result[0] = '\0';
+			}
+			if (link_len > 1)
+			{
+				link[link_len] = '\0';
+				char *last = strrchr(link, '/');
+				*last = '\0';
+			}
+
+			snprintf(tmp_path, 2 * PATH_MAX, "%s/%s", result, link);
+		}
+
+		strncpy(result, tmp_path, PATH_MAX);
 	}
 
 	struct stat path_stat;
 	stat(result, &path_stat);
-	if (S_ISDIR(path_stat.st_mode))
+	if (strlen(result) == 0 || S_ISDIR(path_stat.st_mode))
 	{
+		result[strlen(result) + 1] = '\0';
 		result[strlen(result)] = '/';
 	}
 
