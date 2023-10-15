530919620d1c58ebaf791a5f568865168fd8d9fb
Daria Baranchikova <dariabaranchikova@gmail.com>
2023-10-15 13:59:17 +0300

    add realpath

--- 13-realpath/solution.c
+++ 13-realpath/solution.c
@@ -1,8 +1,49 @@
-#include <solution.h>
+#include "solution.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-void abspath(const char *path)
+void abspath(const char *input)
 {
-	/* implement me */
+	char *path = strdup(input);
 
-	(void) path;
+	if (path == NULL || strlen(path) < 1)
+	{
+		return;
+	}
+
+	char result[PATH_MAX] = "";
+
+	char *token, *saveptr;
+	while ((token = strtok_r(path, "/", &saveptr)) != NULL)
+	{
+		path = NULL;
+
+		char tmp[PATH_MAX] = "";
+		char full_path[PATH_MAX] = "";
+		snprintf(tmp, PATH_MAX, "%s/%s", result, token);
+		realpath(tmp, full_path);
+		if (errno)
+		{
+			report_error(result, token, errno);
+			free(path);
+			return;
+		}
+
+		strncpy(result, full_path, PATH_MAX);
+	}
+
+	struct stat path_stat;
+	stat(result, &path_stat);
+	if (S_ISDIR(path_stat.st_mode))
+	{
+		result[strlen(result)] = '/';
+	}
+
+	free(path);
+	report_path(result);
 }
