87c86a288bb61a63679d6114d33481752867b13c
Daria Baranchikova <dariabaranchikova@gmail.com>
2023-10-28 01:32:44 +0300

    add 14

--- 14-ext2-blkiter/solution.c
+++ 14-ext2-blkiter/solution.c
@@ -1,51 +1,103 @@
-#include <solution.h>
+#include <ext2fs/ext2fs.h>
 #include <fs_malloc.h>
+#include <solution.h>
+#include <unistd.h>
 
 #include <errno.h>
 
+#define BLOCK_SIZE 1024
+
 struct ext2_fs
 {
-	/* implement me */
+	int fd;
+	int inode_table;
 };
 
 struct ext2_blkiter
 {
-	/* implement me */
+	struct ext2_inode inode;
+	int curr;
 };
 
 int ext2_fs_init(struct ext2_fs **fs, int fd)
 {
-	/* implement me */
-	(void) fs;
-	(void) fd;
-	return -EOPNOTSUPP;
+	struct ext2_fs *new_fs = fs_xmalloc(sizeof(struct ext2_fs));
+	new_fs->fd = fd;
+
+	int res = lseek(new_fs->fd, BLOCK_SIZE, SEEK_SET);
+	if (res == -1)
+	{
+		fs_xfree(new_fs);
+		return -errno;
+	}
+
+	struct ext2_super_block super;
+	res = read(new_fs->fd, &super, sizeof(super));
+	if (res == -1)
+	{
+		fs_xfree(new_fs);
+		return -errno;
+	}
+	// Check super MAGIC
+
+	struct ext2_group_desc group;
+	res = lseek(new_fs->fd, BLOCK_SIZE * 2, SEEK_SET);
+	if (res == -1)
+	{
+		fs_xfree(new_fs);
+		return -errno;
+	}
+	res = read(new_fs->fd, &group, sizeof(group));
+	if (res == -1)
+	{
+		fs_xfree(new_fs);
+		return -errno;
+	}
+	// Check group hash
+
+	new_fs->inode_table = group.bg_inode_table;
+	*fs = new_fs;
+	return 0;
 }
 
 void ext2_fs_free(struct ext2_fs *fs)
 {
-	/* implement me */
-	(void) fs;
+	fs_xfree(fs);
 }
 
 int ext2_blkiter_init(struct ext2_blkiter **i, struct ext2_fs *fs, int ino)
 {
-	/* implement me */
-	(void) i;
-	(void) fs;
-	(void) ino;
-	return -EOPNOTSUPP;
+	int res = lseek(fs->fd, fs->inode_table * BLOCK_SIZE + (ino - 1) * sizeof(struct ext2_inode), SEEK_SET);
+	if (res == -1)
+	{
+		return -errno;
+	}
+	struct ext2_blkiter *new_iter = fs_xmalloc(sizeof(struct ext2_blkiter));
+	res = read(fs->fd, &new_iter->inode, sizeof(struct ext2_inode));
+	if (res == -1)
+	{
+		return errno;
+	}
+
+	*i = new_iter;
+	new_iter->curr = 0;
+
+	return 0;
 }
 
 int ext2_blkiter_next(struct ext2_blkiter *i, int *blkno)
 {
-	/* implement me */
-	(void) i;
-	(void) blkno;
-	return 0;
+	int ptr = i->inode.i_block[i->curr];
+	if (ptr == 0)
+	{
+		return 0;
+	}
+	*blkno = ptr;
+	i->curr++;
+	return 1;
 }
 
 void ext2_blkiter_free(struct ext2_blkiter *i)
 {
-	/* implement me */
-	(void) i;
+	fs_xfree(i);
 }
