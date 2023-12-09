771adfba27fc7f6d3ff98b024fa8a7ff18088085
Daria Baranchikova <dariabaranchikova@gmail.com>
2023-12-09 15:55:18 +0300

    try 12

--- 12-grpc-prometheus/pkg/parhash/server.go
+++ 12-grpc-prometheus/pkg/parhash/server.go
@@ -2,10 +2,24 @@ package parhash
 
 import (
 	"context"
+	"fs101ex/pkg/gen/hashsvc"
+	"fs101ex/pkg/gen/parhashsvc"
+	"fs101ex/pkg/workgroup"
+	"net"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/sync/semaphore"
+)
+
+const (
+	promNamespace = "parhash"
 )
 
 type Config struct {
@@ -54,29 +68,164 @@ type Config struct {
 type Server struct {
 	conf Config
 
-	sem *semaphore.Weighted
+	wg   sync.WaitGroup
+	stop context.CancelFunc
+	l    net.Listener
+	sem  *semaphore.Weighted
+
+	// Round Robin clients
+	currClient  atomic.Uint64
+	conns       []*grpc.ClientConn
+	hashClients []hashsvc.HashSvcClient
+
+	requestCounter prometheus.Counter
+	latencyHist    prometheus.HistogramVec // in ms
 }
 
 func New(conf Config) *Server {
+	sem := semaphore.NewWeighted(int64(conf.Concurrency))
+
 	return &Server{
 		conf: conf,
-		sem:  semaphore.NewWeighted(int64(conf.Concurrency)),
+		sem:  sem,
 	}
 }
 
 func (s *Server) Start(ctx context.Context) (err error) {
 	defer func() { err = errors.Wrap(err, "Start()") }()
 
-	/* implement me */
+	if err = s.createHashClients(); err != nil {
+		return err
+	}
+	if err = s.createMetrics(); err != nil {
+		return err
+	}
+
+	ctx, s.stop = context.WithCancel(ctx)
+
+	s.l, err = net.Listen("tcp", s.conf.ListenAddr)
+	if err != nil {
+		return err
+	}
+
+	srv := grpc.NewServer()
+	parhashsvc.RegisterParallelHashSvcServer(srv, s)
+
+	s.wg.Add(2)
+	go func() {
+		defer s.wg.Done()
+
+		srv.Serve(s.l)
+	}()
+	go func() {
+		defer s.wg.Done()
+
+		<-ctx.Done()
+		s.l.Close()
+	}()
 
 	return nil
 }
 
 func (s *Server) ListenAddr() string {
-	/* implement me */
-	return ""
+	return s.l.Addr().String()
 }
 
 func (s *Server) Stop() {
-	/* implement me */
+	s.stop()
+	s.wg.Wait()
+	for _, conn := range s.conns {
+		conn.Close()
+	}
+}
+
+func (s *Server) createHashClients() error {
+	for _, addr := range s.conf.BackendAddrs {
+		conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			return err
+		}
+
+		client := hashsvc.NewHashSvcClient(conn)
+		s.conns = append(s.conns, conn)
+		s.hashClients = append(s.hashClients, client)
+	}
+
+	return nil
+}
+
+func (s *Server) createMetrics() error {
+	requestCounter := prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: promNamespace,
+			Name:      "nr_nr_requests",
+		},
+	)
+
+	buckets := prometheus.ExponentialBucketsRange(0.1, 1000, 24)
+	latencyHist := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: promNamespace,
+			Name:      "subquery_durations",
+			Buckets:   buckets,
+		},
+		[]string{
+			"backend",
+		},
+	)
+
+	err := s.conf.Prom.Register(requestCounter)
+	if err != nil {
+		return err
+	}
+
+	err = s.conf.Prom.Register(latencyHist)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Server) ParallelHash(ctx context.Context, req *parhashsvc.ParHashReq) (*parhashsvc.ParHashResp, error) {
+	s.requestCounter.Inc()
+
+	workers := workgroup.New(workgroup.Config{Sem: s.sem})
+	hashes := make([][]byte, len(req.Data))
+
+	for i, buffer := range req.Data {
+		currSubReq := i
+		currBuff := buffer
+
+		workers.Go(ctx, func(ctx context.Context) error {
+			client, addr := s.getClient(ctx)
+			startTime := time.Now()
+			defer func() {
+				s.latencyHist.
+					With(prometheus.Labels{"backend": addr}).
+					Observe(float64(time.Since(startTime).Milliseconds()))
+			}()
+
+			resp, err := client.Hash(ctx, &hashsvc.HashReq{Data: currBuff})
+			if err != nil {
+				return err
+			}
+
+			hashes[currSubReq] = resp.GetHash()
+			return nil
+		})
+	}
+
+	err := workers.Wait()
+	if err != nil {
+		return nil, err
+	}
+
+	return &parhashsvc.ParHashResp{Hashes: hashes}, nil
+}
+
+func (s *Server) getClient(ctx context.Context) (hashsvc.HashSvcClient, string) {
+	acquired := s.currClient.Add(1) % uint64(len(s.hashClients))
+
+	return s.hashClients[acquired], s.conf.BackendAddrs[acquired]
 }
